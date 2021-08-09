c02fe177cf92ea7e062220b985754bb4aa715ac6
Crowdbotics <team@crowdbotics.com>
2021-08-09 13:31:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aged_surf_29665Identifier",
+  url: "https://aged_surf_29665Identifier.botics.co",
+  api: "https://aged_surf_29665Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
